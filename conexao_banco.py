fa9409239207d821af8c622e98edd9292d3a9a06
Vinícius Azevedo <vmeazevedo@gmail.com>
2021-07-16 20:03:24 -0300

    alteração na conexão com o banco

--- conexao_banco.py
+++ conexao_banco.py
@@ -1,17 +1,27 @@
-###########################################################################
+import mysql.connector
+from mysql.connector import connect, cursor
+
 # Conexão com o banco de dados	
 host = "localhost"
 user = "root"
 password = ""
 
-connection = mysql.connector.connect(
-    host=host,
-    user=user, 
-    password=password)
-###########################################################################
-# Criação da database e tabela
-cursor = connection.cursor()
-cursor.execute("CREATE DATABASE IF NOT EXISTS dados_cadastrais")
-cursor.execute("USE dados_cadastrais")
-cursor.execute("CREATE TABLE IF NOT EXISTS info_cadastrais(id INT AUTO_INCREMENT, Nome VARCHAR(100), CPF VARCHAR(20), Endereço VARCHAR(100), Telefone VARCHAR(20), E-mail VARCHAR(100), Data_Nascimento VARCHAR(100), Idade VARCHAR(4), Sexo VARCHAR(2))")
-###########################################################################
+def connection():
+    try:  
+        connection = mysql.connector.connect(
+            host=host,
+            user=user,
+            password=password
+        )
+        print("\nConexão estabelecida!")
+        cursor = connection.cursor()
+    except mysql.connector.Error as err:
+        print(err)
+    
+    # Criação da database e tabela
+    cursor.execute("CREATE DATABASE IF NOT EXISTS dados_cadastrais")
+    cursor.execute("USE dados_cadastrais")
+    cursor.execute("CREATE TABLE IF NOT EXISTS info_cadastrais(id INT AUTO_INCREMENT, Nome VARCHAR(100), CPF VARCHAR(20), Endereço VARCHAR(100), Telefone VARCHAR(20), Email VARCHAR(100), Data_Nascimento VARCHAR(100), Idade VARCHAR(4), Sexo VARCHAR(2), primary key (id))")
+    print("\nTabela criada com sucesso!")
+
+connection()
---
+++ crud.py
@@ -0,0 +1,9 @@
+from mysql.connector import cursor
+from mysql.connector.cursor import CursorBase
+from conexao_banco import criacao_tabela
+
+criacao_tabela()
+
+
+def create():
+    
